3aea8bb7a09b4a32c27e09e8e7f95823aec77e29
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-04-18 19:55:45 +0700

    Add test for category class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.category import Category
 from src.product import Product
 
 
@@ -11,6 +12,11 @@ def test_product_1() -> Product:
 
 @pytest.fixture
 def test_product_2() -> Product:
-    """Данные для первого объекта класса Product"""
+    """Данные для второго объекта класса Product"""
     return Product("Имя2", "Описание2", 102, 12)
+
+@pytest.fixture
+def test_product_3() -> Product:
+    """Данные для третьего объекта класса Product"""
+    return Product("Имя3", "Описание3", 103, 13)
 
