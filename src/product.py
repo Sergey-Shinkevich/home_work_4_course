6337990cf52e19f33b9c57ac4b78c04fbf361f98
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-04-18 18:48:35 +0700

    Add __init__ for product class

--- src/product.py
+++ src/product.py
@@ -1,9 +1,12 @@
-
-
 class Product:
     """Объявление класса продукта"""
     name: str
     description: str
     price: float
     quantity: int
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
 
