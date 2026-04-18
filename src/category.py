ef80e3c785787be3cb9363fbd210daa531d2de32
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-04-18 19:03:05 +0700

    Add __init__ for category class

--- src/category.py
+++ src/category.py
@@ -2,6 +2,14 @@ from src.product import Product
 
 class Category:
     """Объявление класса Category"""
+    category_count = 0
+    product_count = 0
     name: str
     description: str
     products: list[Product]
+    def __init__(self, name: str, description: str, products: list[Product]) -> None:
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
