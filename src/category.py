9d556db70207d490f12345c01fc9a1897f87f274
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-04-28 02:50:37 +0700

    Add all methods and functions

--- src/category.py
+++ src/category.py
@@ -3,17 +3,32 @@ from src.product import Product
 
 class Category:
     """Объявление класса Category"""
-
     category_count = 0
     product_count = 0
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
-    def __init__(self, name: str, description: str, products: list[Product]) -> None:
+    def __init__(self, name: str, description: str, products: list) -> None:
         """Конструктор класса Category"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
+        Category.product_count += len(self.__products)
         Category.category_count += 1
-        Category.product_count += len(self.products)
+
+
+    def add_product(self, item: Product) -> None:
+        """Метод добавления нового Product в Category"""
+        self.__products.append(item)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Геттер атрибута products"""
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
+
+
